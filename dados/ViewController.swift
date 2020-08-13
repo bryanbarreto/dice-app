a8e441f548ffa55314678f4a4525cd8e35be86ec
bryan barreto <bryan.barreto@icloud.com>
2020-08-13 00:34:02 -0300

    projeto finalizdo

--- dados/ViewController.swift
+++ dados/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    /* Array com todas as imagens dos dados*/
+    let diceArray:Array<String> = ["one","two","three","four","five","six","zero"]
+    
+    @IBOutlet weak var dice1ImageView: UIImageView!
+    @IBOutlet weak var dice2ImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       /* Codigo que executa quando a tela é carregada */
+        dice1ImageView.image = #imageLiteral(resourceName: "zero")
+        dice2ImageView.image = #imageLiteral(resourceName: "zero")
     }
 
-
+    @IBAction func rollDiceButtonPress(_ sender: UIButton) {
+        /* funcao de click do botao */
+        dice1ImageView.image = UIImage(named: diceArray[randomNumber()])
+        dice2ImageView.image = UIImage(named: diceArray[randomNumber()])
+    }
+    
+    func randomNumber()->Int{
+        return Int.random(in: 0...5)
+    }
+    
 }
 
